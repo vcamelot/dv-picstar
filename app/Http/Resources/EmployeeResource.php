649340f16036f77384ba979a540fa49a3569ac3a
Dmitry Vinogradov <vinogradov.dmitry@gmail.com>
2021-10-19 23:20:17 +0300

    Add Employee resource

---
+++ app/Http/Resources/EmployeeResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'position' => $this->position,
+            'superior' => $this->superior,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date
+        ];
+    }
+}
