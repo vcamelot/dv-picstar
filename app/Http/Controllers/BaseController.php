408339784c083de246d75cc18d2bffb7d752af73
Dmitry Vinogradov <vinogradov.dmitry@gmail.com>
2021-10-19 23:31:23 +0300

    Add Base controller and Employee controller

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function __construct()
+    {
+    }
+
+    public function SuccessResponse($data, $message = 'OK'): JsonResponse
+    {
+        return response()->json([
+            $message,
+            'data' => 'data'
+        ], 200);
+    }
+
+    public function ErrorResponse($data, $message = 'Error', $errorCode = 404): JsonResponse
+    {
+        return response()->json([
+            $message,
+            'data' => 'data'
+        ], $errorCode);
+
+    }
+}
---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\EmployeeResource;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\Employee;
+
+class EmployeeController extends BaseController
+{
+    public function show($id): JsonResponse
+    {
+        $employee = Employee::find($id);
+        if (is_null($employee)) {
+            return $this->ErrorResponse([], 'Employee not found');
+        }
+
+        return $this->SuccessResponse(new EmployeeResource($employee));
+    }
+}
